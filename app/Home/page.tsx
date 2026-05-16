f1310ec8eb669d0d3f7029e3877b66de9b494969
M.saleem <Muhammad.Saleem@senew-tech.com>
2026-05-16 10:26:05 +0500

    update changes sport

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>
+        Welcome to the Sports API Provider! Our platform offers real-time sports data, including live scores, fixtures, and player statistics. Whether you're a developer building a sports app or a fan looking for up-to-date information, our API has you covered. Stay tuned for more features and updates as we continue to enhance our services!  
+      </p>
     </div>
   )
 }
